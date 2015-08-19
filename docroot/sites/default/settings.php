7c26deb23e1b8219db013a5c8f3706801f396db3
Mark Carver <mark.carver@me.com>
2015-08-19 18:55:24 -0500

    Use D8 style local settings

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -552,4 +552,16 @@ $conf['404_fast_html'] = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN"
  */
 # $conf['allow_authorize_operations'] = FALSE;
 
-@include 'local.settings.php';
+/**
+ * Load local development override configuration, if available.
+ *
+ * Use settings.local.php to override variables on secondary (staging,
+ * development, etc) installations of this site. Typically used to disable
+ * caching, JavaScript/CSS compression, re-routing of outgoing emails, and
+ * other things that should not happen on development and testing sites.
+ *
+ * Keep this code block at the end of this file to take full effect.
+ */
+if (file_exists(__DIR__ . '/settings.local.php')) {
+  include __DIR__ . '/settings.local.php';
+}
