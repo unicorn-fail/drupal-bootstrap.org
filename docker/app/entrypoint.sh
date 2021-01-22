cf9c5e58edc29bd2b8e647cd79594191e3ec37f7
Hunter Lannon <hunter.d.lannon@gmail.com>
2021-01-22 16:48:43 -0600

    adding group write to /var/run/apache2
    Apache was giving up running because it was unable to create its pid
    file at /var/run/apache2/apache2.pid

--- docker/app/entrypoint.sh
+++ docker/app/entrypoint.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-
-touch /var/log/apache2/bootstrap-access.log /var/log/apache2/bootstrap-error.log 
-
 # Put access logs into container's stdout
 tail -f /var/log/apache2/bootstrap-access.log &
 
