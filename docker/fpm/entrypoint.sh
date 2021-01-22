6b8a94351df974247e7b0b1e4218e4adb45415f9
Hunter Lannon <hunter.d.lannon@gmail.com>
2021-01-22 15:52:58 -0600

    Switch logs to file + tail instead of using /proc
    Running containers on OpenShift implies that you need to be able to run
    the container as a non-root user. Non-root users can not open stdout and
    stderr file descriptors via /proc/self/fd/{1,2} unless they are part of
    the tty group.
    
    We can't add that group to the user in the Dockerfile
    since the user/uid is determined at runtime by OpenShift.
    
    We can't add the user to that group in the entrypoint since we'd have to
    re-login and a non-root user can't change their own group membership
    anyway
    
    So for OpenShift, we will use log file + tail like are with the 'app'
    container that will be communicating with the fpm container

--- docker/fpm/entrypoint.sh
+++ docker/fpm/entrypoint.sh
@@ -3,19 +3,12 @@
 # Create missing PIDfile dir \
 mkdir -p /run/php/
 
-# Configure logging
-{ \
-    echo 'catch_workers_output = yes'; \
-    echo 'access.log = /proc/self/fd/2'; \
-    echo 'php_admin_value[error_log] = /proc/self/fd/2'; \
-    echo 'php_admin_flag[log_errors] = on'; \
-    echo 'php_flag[display_errors] = off'; \
-    echo 'php_admin_value[display_startup_errors] = on'; \
-    echo 'php_admin_value[error_reporting] = E_ALL & ~E_NOTICE & ~E_WARNING & ~E_STRICT & ~E_DEPRECATED'; \
-} >> /etc/php/$PHP_VERSION/fpm/pool.d/www.conf
-
 # Force FPM to listen on localhost:9000
 sed -i 's/\/run.*sock/127.0.0.1:9000/' /etc/php/$PHP_VERSION/fpm/pool.d/www.conf
 
+# Output logs to proper streams
+tail -f /var/log/php_access.log &
+tail -f /var/log/php_error.log 1>&2 &
+
 # Start FPM
-/usr/sbin/php-fpm${PHP_VERSION} -F --fpm-config /etc/php/$PHP_VERSION/fpm/php-fpm.conf
+exec /usr/sbin/php-fpm${PHP_VERSION} -F --fpm-config /etc/php/$PHP_VERSION/fpm/php-fpm.conf
