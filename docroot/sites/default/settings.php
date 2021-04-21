d96f8fe3a8cf48646b5699a211139b87037a30e3
Mark Halliwell <mark@unicorn.fail>
2021-04-21 12:50:38 -0500

    Add ddev

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -613,3 +613,9 @@ $conf['preprocess_js'] = 1;
 if (file_exists(__DIR__ . '/settings.local.php')) {
   include __DIR__ . '/settings.local.php';
 }
+
+// Automatically generated include for settings managed by ddev.
+$ddev_settings = dirname(__FILE__) . '/settings.ddev.php';
+if (is_readable($ddev_settings) && getenv('IS_DDEV_PROJECT') == 'true') {
+  require $ddev_settings;
+}
